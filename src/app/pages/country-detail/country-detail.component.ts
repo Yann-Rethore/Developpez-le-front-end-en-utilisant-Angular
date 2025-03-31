4d851c57a1e53aabefac2aa95ebb79b0cd876639
Yann-Rethore <yann.rethore.pin@gmail.com>
2025-03-31 21:15:18 +0200

    refactoring et mise en page

--- src/app/pages/country-detail/country-detail.component.ts
+++ src/app/pages/country-detail/country-detail.component.ts
@@ -1,8 +1,9 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 import { OlympicsData } from 'src/app/core/models/Olympic';
-
+import { Subject, of } from 'rxjs';
+import { takeUntil, catchError } from 'rxjs/operators';
 
 
 @Component({
@@ -11,43 +12,64 @@ import { OlympicsData } from 'src/app/core/models/Olympic';
   styleUrl: './country-detail.component.scss',
   standalone : false
 })
-export class CountryDetailComponent implements OnInit {
-  view: [number, number] = [700, 400]; // Dimensions par défaut
+export class CountryDetailComponent implements OnInit, OnDestroy {  
+  view: [number, number] = [700, 400]; // Dimensions par défaut du graphique
   countryData: OlympicsData |  undefined;
   chartData: {name: string; series: {name: string; value: number}[]}[] = [];
   totalParticipations: number = 0;
   totalMedals: number = 0;
   totalAthletes: number = 0;
 
+  private destroy$ = new Subject<void>(); // Subject pour gérer les désabonnements
 
-  constructor(private route: ActivatedRoute,
+  constructor(
+    private route: ActivatedRoute,
     private olympicService: OlympicService
   ) { }
 
   ngOnInit(): void {
+    // Récupération de l'ID du pays depuis l'URL
+    // et appel du service pour récupérer les données des JO
     const countryId = Number(this.route.snapshot.paramMap.get('id'));
 
-    this.olympicService.getOlympics().subscribe((data: OlympicsData[]) => {
+    this.olympicService
+    .getOlympics()
+    .pipe(takeUntil(this.destroy$), // Désabonnement automatique lors de la destruction
+    catchError((error) => {
+      console.error('Erreur lors de la récupération des données des JO:', error);
+      return of([]); // Retourne un tableau vide en cas d'erreur
+    }))
+    .subscribe((data: OlympicsData[]) => {
       this.countryData = data.find((item) => item.id === countryId);
-      console.log(this.countryData); // Affiche les données du pays sélectionné
-      this.chartData = [{
-        name: this.countryData?.country || '',
-        series: this.countryData?.participations.map((participation) => ({
-          name: participation.year.toString(),
-          value: participation.medalsCount
-        })) || []
-      }];
-    this.totalParticipations = this.countryData?.participations.length || 0;
+      if (this.countryData) {  
+        this.chartData = [
+          {
+            name: this.countryData?.country ,
+            series: (this.countryData?.participations).map((participation) => ({
+              name: participation.year.toString(),
+              value: participation.medalsCount
+            }))
+          }
+        ];
+        this.totalParticipations = this.countryData?.participations.length;
 
-    this.totalMedals = this.countryData?.participations.reduce((sum, participation) => 
-      sum + participation.medalsCount, 0) || 0;
+        this.totalMedals = this.countryData?.participations.reduce((sum, participation) => 
+          sum + participation.medalsCount, 0
+        );
 
-    this.totalAthletes = this.countryData?.participations.reduce((sum, participation) =>
-      sum + participation.athleteCount, 0) || 0;
-    })
+        this.totalAthletes = this.countryData?.participations.reduce((sum, participation) =>
+          sum + participation.athleteCount, 0
+        );
+      } else {
+        // Si aucune donnée n'est trouvée pour le pays
+        this.chartData = [];
+        this.totalParticipations = 0;
+        this.totalMedals = 0;
+        this.totalAthletes = 0;
+      }
+   });
 
-
-    this.updateChartSize(); // Met à jour la taille au chargement
+    this.updateChartSize(); // Met à jour la taille au graphique au chargement
 
   }
 
@@ -62,6 +84,10 @@ export class CountryDetailComponent implements OnInit {
     this.view = [width, height];
   }
 
+  ngOnDestroy(): void {
+    this.destroy$.next(); // Émet un signal pour arrêter les abonnements
+    this.destroy$.complete(); // Termine le Subject
+  }
   
 }
 
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,10 @@
 import { Component, HostListener, OnInit } from '@angular/core';
-import { count, map, tap, catchError } from 'rxjs/operators';
-import { Observable, of, partition } from 'rxjs';
+import { map, tap, catchError } from 'rxjs/operators';
+import { Observable, of} from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 import { Router } from '@angular/router';
 import { ChartOlympicsData, OlympicsData } from 'src/app/core/models/Olympic';
-import { AppRoutingModule } from 'src/app/app-routing.module';
+
 
 @Component({
     selector: 'app-home',
@@ -13,35 +13,22 @@ import { AppRoutingModule } from 'src/app/app-routing.module';
     standalone: false
 })
 export class HomeComponent implements OnInit {
-  public olympics$: Observable<ChartOlympicsData[]> = of([
-    {
-      name: '',
-      value: 0,
-      extra: {
-      id: 0,
-      country: '',
-      participations: [],
-      }
-    }
-  ]
-  )
-  public olympicsDatas$: Observable<OlympicsData[]> = new Observable();
+  public olympics$: Observable<ChartOlympicsData[]> = new Observable(); // Observable pour stocker les données des JO
   public totalCountries: number =0;
   public nombreDeJo: number = 0;
 
-  public view: [number, number] = [700, 400]; // Dimensions par défaut
+  public view: [number, number] = [700, 400]; // Dimensions par défaut du graphique
 
-  constructor(private olympicService: OlympicService,
-    private olympicService2: OlympicService,
+  constructor(
+    private olympicService: OlympicService,
     private router: Router
   ) {}
 
-  ngOnInit(): void {
-    
+  ngOnInit(): void { 
+    // Appel du service pour récupérer les données des JO , gérer en asynchrone donc de OnDestroy   
+    // et transformation des données pour le graphique
     this.olympics$ = this.olympicService.getOlympics().pipe(
       map((data:OlympicsData[]) =>
-        
-        
         data.map((item) => ({
           name: item.country,
           value: item.participations.reduce(
@@ -50,8 +37,7 @@ export class HomeComponent implements OnInit {
           extra: {
           id: item.id,
           country: item.country,
-          participations: item.participations
-          
+          participations: item.participations         
           }
         }))
       ),
@@ -60,8 +46,11 @@ export class HomeComponent implements OnInit {
         this.nombreDeJo = Math.max(
           ...data.map((country) => country.extra.participations.length) 
         );
-        console.log('Total Countries:', this.totalCountries);
-        console.log('Max Participations:', this.nombreDeJo);
+      }),
+      // en cas d'erreur, renvoyer un tableau vide
+      catchError((error) => {
+        console.error('Erreur lors de la récupération des données des JO:', error);
+        return of([]); 
       })
     );
     this.updateChartSize(); // Met à jour la taille au chargement
@@ -78,10 +67,6 @@ export class HomeComponent implements OnInit {
     this.view = [width, height];
   }
   
-    
-
-  
-
   onSelect(data: ChartOlympicsData): void {
     console.log('Contry clicked:' ,data)
   
